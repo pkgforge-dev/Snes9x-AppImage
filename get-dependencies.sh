b4409796c82f913edafccf2bba0cd19ce5ddf7a7
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:51:06 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -28,10 +28,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package
-
-# If the application needs to be manually built that has to be done down here
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
     echo "Making nightly build of Snes9x-GTK..."
     echo "---------------------------------------------------------------"
