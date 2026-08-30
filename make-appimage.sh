50ddad7b671eb2025f76d911946ff97beeaec508
Link4Electronics <link4electronics@gmail.com>
2026-08-30 05:39:10 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -19,3 +19,7 @@ quick-sharun /usr/bin/snes9x-gtk
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
